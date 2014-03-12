bffd6560176da1f8b98778e73f5f74947b6d44ea
Luis Eduardo Gonzalez O <lgonzalez@energiasolarsa.com>
2014-03-12 12:06:12 -0500

    Initial commit and install foundation5

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '22a3c018c1257ee8d6943c7b2de6345ed08e033801ef0cece53c641943c160c65753571c9742ea068da6e3d95bde772c67d16c10e67e5961b984cf303e1cae9c'
