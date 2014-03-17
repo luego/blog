ecb27d43687900314a8eb6e429d15ca783cb0e42
Luis Edo Gonzalez O <luego1986@gmail.com>
2014-03-17 18:53:00 +0000

    Add comments model and controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ActiveRecord::Base
-  #attr_accessible :content, :name, :tag_list
   acts_as_taggable # Alias for acts_as_taggable_on :tags
   belongs_to :user
   belongs_to :category
-
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140313043602) do
+ActiveRecord::Schema.define(version: 20140317184651) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "comments", force: true do |t|
+    t.text     "content"
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "posts", force: true do |t|
     t.string   "title"
